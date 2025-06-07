4ef65cb6f3d5e51b70edf471b2e388e3873b15af
Hollow4U <nanishinde123@gmail.com>
2025-06-06 19:03:10 -0500

    Vida del jugador y recibir daño, con evento, observer y action

---
+++ My project (2)/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamage
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    public static event Action OnPlayerDeath; 
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+   /* private void Update()
+    {
+        TakeDamage(0);
+    } */
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        Debug.Log($"Jugador recibió daño: {amount}. Vida actual: {currentHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("¡Jugador ha muerto!");
+        OnPlayerDeath?.Invoke();
+        gameObject.SetActive(false);
+    }
+}
